79af0cc9be9f11ba28d4dce43e240f1bb325b708
Jorge Candamil <jorl.8907@gmail.com>
2021-10-04 15:44:05 -0500

    Cambio texto en boton de inicio de sesion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                 <li className= "px-3">
 
                     <Link to='/login'>
-                    <button className= "bg-green-600 p-2 rounded-lg shadow-md hover:bg-grey-600">Iniciar Sesión</button>
+                    <button className= "bg-green-600 p-2 text-white rounded-lg shadow-md hover:bg-grey-600">Iniciar Sesión</button>
 
                     
                     </Link>
